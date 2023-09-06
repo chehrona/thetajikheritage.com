1326803e025cfcf182066a7f8ed7b89dc7c37669
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2023-09-06 13:07:28 -0400

    tablet

--- src/components/poet/poetBio/poetBioStyles.js
+++ src/components/poet/poetBio/poetBioStyles.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
+import { device } from "../../common/deviceSizes/size";
 import { IconButton } from '@mui/material';
 
 export const MainContainer = styled.div`
     background-image: url(${'/noise.png'});
     min-height: fit-content;
     padding-bottom: 3rem;
+
+    @media (max-width: 768px) { 
+    
+    }
 `;
 
 // Box one ****************************
@@ -15,6 +20,12 @@ export const BoxOne = styled.div`
     padding: 0rem 4.3rem 1rem 5rem;
     color: #0F0A00;
     gap: 3rem;
+    flex-wrap: wrap;
+
+    @media (max-width: 768px) { 
+        padding: 0rem 2rem 0rem 2rem;
+        gap: 0rem;
+    }
 `;
 
 export const Year = styled.div`
@@ -22,11 +33,20 @@ export const Year = styled.div`
     font-family: "EB Garamond", serif;
     margin-bottom: 1rem;
     text-align: ${({ align }) => align && 'center'};
+
+    @media (max-width: 768px) {
+        font-size: 2.5rem;
+        margin-bottom: 0.5rem;
+    }
 `;
 
 export const Desc = styled.div`
     font-size: 1.1rem;
     color: #333333;
+
+    @media (max-width: 768px) {
+        font-size: 1rem;
+    }
 `;
 
 // Box two ****************************
@@ -39,6 +59,11 @@ export const BoxTwo = styled.div`
     align-items: center;
     position: relative;
     text-shadow: 0.0625rem 0.0625rem 0.1875rem #0F0A00;
+
+    @media (max-width: 768px) { 
+        margin-top: 3rem;
+        margin-bottom: 5rem;
+    }
 `;
 
 export const InnerOverlay = styled.div`
@@ -48,6 +73,10 @@ export const InnerOverlay = styled.div`
     width: 100%;
     overflow: hidden;
     filter: grayscale(1);
+
+    @media (max-width: 768px) { 
+        height: ${({ four }) => four ? '20rem' : '35rem'};
+    }   
 `;
 
 export const Backdrop = styled.div`
@@ -61,9 +90,13 @@ export const Backdrop = styled.div`
 
 export const Slides = styled.div`
     position: absolute;
-    left: 10%;
+    left: 5%;
     top: -4rem;
     z-index: 1;
+
+    @media (max-width: 768px) { 
+        top: -0.1%;
+    }
 `;
 
 export const SlideImg = styled.img`
@@ -72,6 +105,10 @@ export const SlideImg = styled.img`
     filter: grayscale(1) ${({ show }) => show ? 'brightness(100%)': 'brightness(60%)'};
     box-shadow: ${({ show }) => 
         show ? '-0.3rem 0rem 0.3rem -0.2rem #dedbdb, 0.3rem 0rem 0.3rem -0.2rem #dedbdb' : '0rem 0rem 1rem 0.2rem #504221e6'};
+
+    @media (max-width: 768px) { 
+        width: 14rem;
+    }
 `;
 
 export const NavBox = styled.div`
@@ -81,6 +118,10 @@ export const NavBox = styled.div`
     ${({ bottom }) => bottom ? 'bottom: 0rem' : 'top: 4rem'};
     right: 0rem;
     z-index: 10;
+
+    @media (max-width: 768px) { 
+        height: 8.5rem;
+    }
 `;
 
 export const Line = styled.div`
@@ -92,6 +133,10 @@ export const Line = styled.div`
     transform: translateX(-50%);
     bottom: ${({ bottom }) => bottom && '0rem'};
     top: ${({ top }) => top && '0rem'};
+
+    @media (max-width: 768px) { 
+        height: 5.5rem;
+    }
 `;
 
 export const StyledIconButton = styled(IconButton)`
@@ -101,6 +146,10 @@ export const StyledIconButton = styled(IconButton)`
     left: 50%;
     transform: translateX(-50%) ${({ bottom }) => bottom ? 'rotate(90deg)' : 'rotate(-90deg)'};
     ${({ bottom }) => bottom ? 'bottom: 0rem' : 'top: 6.5rem'};
+
+    @media (max-width: 768px) { 
+        ${({ bottom }) => bottom ? 'bottom: 0rem' : 'top: 5rem'};
+    }
 `;
 
 export const Arrow = styled.div`
@@ -125,17 +174,31 @@ export const LineWrapper = styled.div`
     height: 20rem;
     width: 100%;
     transition: ease 1000ms;
+
+    @media (max-width: 768px) { 
+        height: 15rem;
+    }
 `;
 
 export const Info = styled.div`
     padding-right: 7%;
     padding-left: 7%;
+
+    @media (max-width: 768px) {
+        padding-right: 2rem;
+        padding-left: 2rem;
+    }
 `;
 
 export const Text = styled.div`
     text-align: justify;
     font-size: 1.1rem;
     line-height: 1.8rem;
+
+    @media (max-width: 768px) {
+        font-size: 1rem;
+        line-height: 1.2rem;
+    }
 `;
 
 // Box three ****************************
@@ -153,6 +216,11 @@ export const QuoteWrapper = styled.div`
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
+
+    @media (max-width: 768px) {
+        width: 4rem;
+        height: 4rem;
+    }
 `;
 
 export const QuoteSymbol = styled.div`
@@ -174,6 +242,12 @@ export const Quote = styled.div`
     position: absolute;
     top: 1rem;
     margin: 5rem;
+
+    @media (max-width: 768px) {
+        font-size: 1.8rem;
+        top: 1rem;
+        margin: 3.5rem;
+    }
 `;
 
 export const DescWrapper = styled.div`
@@ -187,6 +261,13 @@ export const DescWrapper = styled.div`
     top: 9rem;
     font-size: 1.1rem;
     font-family: 'IBM Plex Serif', serif;
+
+    @media (max-width: 768px) {
+        column-count: 1;
+        padding: 3.5rem 2rem 2rem 2rem;
+        font-size: 1rem;
+        max-height: 100%;
+    }
 `;
 
 // Box four *****************************
@@ -203,6 +284,11 @@ export const YearBig = styled.div`
     margin-bottom: 1rem;
     text-align: ${({ align }) => align && 'center'};
     color: ${({ color }) => color && '#bd9d52'};
+
+    @media (max-width: 768px) {
+        font-size: 3.5rem;
+        line-height: 1.5rem;
+    }
 `;
 
 export const TextWrapper = styled.div`
@@ -261,6 +347,10 @@ export const LeftContainer = styled.div`
     width: 50%;
     position: relative;
     height: 100%;
+
+    @media (max-width: 768px) { 
+        width: 100%;
+    }
 `;
 
 export const RightContainer = styled.div`
@@ -271,6 +361,11 @@ export const RightContainer = styled.div`
     background-position: center center;
     transition: all 0.5s;
     position: relative;
+
+    @media (max-width: 768px) { 
+        width: 100%;
+        height: 22rem;
+    }
 `;
 
 export const Overlay = styled.div`
@@ -339,4 +434,8 @@ export const ImgInfo = styled.div`
     padding-left: 0.5rem;    
     color: ${({ color }) => color && color};
     bottom: ${({ up }) => up ? '3.2rem' : '0.5rem'};
+
+    @media (max-width: 768px) { 
+        bottom: ${({ up }) => up ? '0rem' : '0.5rem'};
+    }
 `;
--- src/finalPages/poetPage/poetPageStyles.js
+++ src/finalPages/poetPage/poetPageStyles.js
@@ -7,6 +7,10 @@ export const PageContainer = styled.div`
     margin: 2rem 4.5rem 2rem 4.5rem;
     background-color: #fcf6e9;
     border-radius: 4rem;
+
+    @media (max-width: 768px) { 
+        margin: 1.2rem 2rem 1.2rem 1.2em;
+    }
 `;
 
 export const RefContainer = styled.div`
@@ -15,4 +19,9 @@ export const RefContainer = styled.div`
     background-image: url(${'/noise.png'});
     padding: 3rem;
     padding-top: 0rem;
+
+    @media (max-width: 768px) { 
+        padding: 2rem;
+        padding-top: 0rem;
+    }
 `;
