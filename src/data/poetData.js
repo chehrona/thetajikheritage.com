b5c7a9ad3abd51cc990897b7f3baf79229f8d93f
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2023-09-22 03:37:30 -0400

    Update poetData.js

--- src/data/poetData.js
+++ src/data/poetData.js
@@ -539,7 +539,7 @@ module.exports.poets = [
                     {title: "Образцы таджикской литературы", cover: "/bookCovers/ayni/ruExample.png"}, {title: "История Бухарской революции", cover: "/bookCovers/ayni/ruHistory.png"}, 
                     {title: "Бухарские палачи", cover: "/bookCovers/ayni/ruExec.png"}, {title: "Одина", cover: "/bookCovers/ayni/ruOdina.png"},
                     {title: "Дохунда", cover: "/bookCovers/ayni/ruDokhunda.png"},
-                    {title: "Рабы", cover: "/bookCovers/ayni/ruSlaves.png", link: "17sLG3xU1jaG4c_9-n6MbcI4cM4fqv88F"}, 
+                    {title: "Рабы", cover: "/bookCovers/ayni/ruSlaves.png", link: "1fu9EiIEVAkkqEh1YC0JBKzv6sda1tS5Z"}, 
                     {title: "Смерть ростовщика", cover: "/bookCovers/ayni/ruUsurer.png", link: "1WHlYzyfLihYruu2XnqN7pi5l-mKFKLcZ"},
                     {title: "Сирота", cover: "/bookCovers/ayni/ruOrphan.png"},
                     {title: "Воспоминания", cover: "/bookCovers/ayni/ruMemoirs.png"}, {title: "Собрание сочинений", cover: "/bookCovers/ayni/ruComplete.png"}, 
