4cffacdd2f7d4c7d4c170cdeed4b79bd3202399c
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2023-11-11 21:51:37 -0500

    Update tooltipStyles.js

--- src/components/common/tooltip/tooltipStyles.js
+++ src/components/common/tooltip/tooltipStyles.js
@@ -8,37 +8,8 @@ export const StyledContainer = styled.div`
     margin: 0.5rem;
     font-weight: normal;
     background-color: #70654a;
-    font-size: inherit;
+    font-size: 1.1rem;
     font-family: inherit;
     width: 20rem;
     min-width: 20rem;
-
-    &::after {
-        content: "";
-        top: 97%;
-        left: 50%;
-        position: absolute;
-        border-width: 0.5rem;
-        border-style: solid;
-        transition: opacity 0.3s;
-        border-color: #70654a transparent transparent transparent;
-    }
-
-    ${({ showBottom }) => showBottom && `
-        &::before {
-            content: "";
-            bottom: 97%;
-            left: 50%;
-            position: absolute;
-            margin-left: -0.5rem;
-            border-width: 0.5rem;
-            border-style: solid;
-            transition: opacity 0.3s;
-            border-color: transparent transparent #70654a transparent;
-        }
-
-        &::after {
-            display: none;
-        }
-    `}
 `;
